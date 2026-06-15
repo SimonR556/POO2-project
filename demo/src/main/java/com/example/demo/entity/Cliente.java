469a5f50ff60051d4547a163147a7437f58840e8
SimonR556 <114199272+SimonR556@users.noreply.github.com>
2026-06-15 19:01:39 -0300

    fix: classe Cliente sem marcacoes de entidade

--- demo/src/main/java/com/example/demo/entity/Cliente.java
+++ demo/src/main/java/com/example/demo/entity/Cliente.java
@@ -1,5 +1,15 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Cliente extends Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
 }
